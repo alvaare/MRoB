cbb60e44162b67dd583e47f587e7f47754da1f8d
alvaare <alvaare@gmail.com>
2020-03-09 19:09:04 -0400

    adapting graph structure to future algorithms

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,21 +1,21 @@
 #ifndef __GRAPH_HPP_INCLUDED__
 #define __GRAPH_HPP_INCLUDED__
 
-#include<vector>
+#include<set>
 using namespace std;
 
-typedef pair<int, int> arc;
+typedef pair<int, int> edge;
 
 struct graph {
   int n;
-  vector<arc>* edges;
-  graph(int n) {
-    this->n = n;
-    this->edges = new vector<pair<int,int>>[n];
-  }
-  ~graph() {
-    delete [] this->edges;
-  }
+  set<int>* neighbors;
+  double** distance;
+
+  graph(int n);
+  ~graph();
+  void add_edge(int, int, double);
+  void print();
+  bool is_edge(int, int);
 };
 
 
