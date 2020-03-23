31d23a5298efffa1cb08f088cea474b4096bf6ec
alvaare <alvaare@gmail.com>
2020-03-23 19:56:37 +0100

    changing moats for auxiliary graph, black hole point of view

--- include/steiner_forest.hpp
+++ include/steiner_forest.hpp
@@ -4,12 +4,21 @@
 #include"graph.hpp"
 #include"sample-augment.hpp"
 
-struct moat {
-  set<int> nodes;
-  set<edge> out_edges;
+struct aux_graph {
+  graph G;
+  map<int, set<int>> aux_nodes;
+  map<int, int> father;
+  map<edge, edge> memory_edge;
+  set<int> active_nodes;
 
-  bool operator==(const moat) const;
-  size_t operator()(moat);
+  set<int>* from_node_to_set(int);
+};
+
+struct aux_node {
+  int id;
+  double dual_value = 0;
+
+  bool operator<(const aux_node) const;
 };
 
 void steiner_forest(instance, sample*, solution);
